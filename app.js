73800bdd11537ad9157a81369303f56bf04400dd
Vinicius2016 <vini_@vini2000>
2023-08-02 17:48:41 -0300

    voltando para o estado anterior

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 consolo.log("aplicação esta se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("O paulo fez esse commit sem voce ver na aula!")
